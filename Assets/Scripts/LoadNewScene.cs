22c31c9e8475c7df95c75d58781a2408b4656069
Kendoschl <60159000+Kendoschl@users.noreply.github.com>
2020-11-07 20:31:28 -0500

    WebGL Build

---
+++ Assets/Scripts/LoadNewScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewScene : MonoBehaviour
+{
+    public void LoadTerrain()
+    {
+        SceneManager.LoadScene("Terrain");
+    }
+
+    public void LoadInterior()
+    {
+        SceneManager.LoadScene("Interior");
+    }
+
+    public void LoadTexture()
+    {
+        SceneManager.LoadScene("Texture");
+    }
+
+    public void LoadPrototype()
+    {
+        SceneManager.LoadScene("Prototype 2");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
